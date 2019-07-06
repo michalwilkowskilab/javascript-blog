62b7dba3d6b41e2e39e5678b6613a6cb59b94d22
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-07-06 12:33:30 +0200

    Remove class .active form class .post.active

--- js/script.js
+++ js/script.js
@@ -4,11 +4,26 @@ function titleClickHandler(event){
   console.log('Link was clicked!',event);
 
   /*remove class 'active' from all article links */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
   /* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.post.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+
   /* get 'href' attribute frorm the clicked link */
   /* find the correct article uisng the selector (value of 'herf' attribute) */
   /* add class 'active' to the correct article */ 
+
 }
 
 const links = document.querySelectorAll('.titles a');
