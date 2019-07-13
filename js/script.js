54f1be25b0a71ea9320a3a1fcb043ccd5a35bf6a
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-07-13 18:13:46 +0200

    Generate code into html

--- js/script.js
+++ js/script.js
@@ -141,7 +141,8 @@ function generateTags(){
     for(let tag of articleTagsArray){
     console.log(tag);
       /* generate HTML of the link */
-      
+      const linkHTML = '<li><a href="'+ articleTags +'">'+ articleTagsArray +'</a></li>';
+      console.log(linkHTML)
       /* add generated code to html variable */
     
     /* END LOOP: for each tag */
